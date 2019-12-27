f70a3ff3a3f191e6a927a492261ca01f13560db6
Lorenzo Fattori <lfattori@procentec.com>
2019-12-27 19:33:27 +0100

    divide channel - midi type - note and value

--- midiTest.py
+++ midiTest.py
@@ -11,8 +11,8 @@ import time
 
 from rtmidi.midiutil import open_midiinput
 
-log = logging.getLogger("midiin_callback")
-logging.basicConfig(level=logging.DEBUG)
+# log = logging.getLogger("midiin_callback")
+# logging.basicConfig(level=logging.DEBUG)
 
 
 class MidiInputHandler(object):
@@ -21,9 +21,28 @@ class MidiInputHandler(object):
         # self._wallclock = time.time()
 
     def __call__(self, event, data=None):
-        message, * = event
+        message, deltatime = event
         # self._wallclock += deltatime
+
+        # message[0] is a combination of type of midi and channel
+        # Example: 0x90 = note On - channel 1
+        # 0x80 = note off - channel 1
+        # 0x91 = note on - channel 2
+        # 0xb0 = control change - channel 1
+        # 0x9f = note on - channel 16
+        # here I split them separately
+
+        # WARNING - channel goes from 0 to 15 not from 1 to 16!
+        channel = message[0] & 0x0F
+        # this is in decimal, if you want to print 0x80 you need to convert it in hex
+        midiType = message[0] & 0xF0
+        note = message[1]
+        value = message[2]
+
         print("[%s] %r" % (self.port, message))
+        print(
+            f"Channel: {channel}, type: {hex(midiType)}, note: {note}, value: {value}"
+        )
 
 
 # Prompts user for MIDI input port, unless a valid port number or name
