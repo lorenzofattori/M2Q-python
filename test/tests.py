3eeb84c3cf4e4937fdc9348725ca21714e0f9d15
Lorenzo Fattori <25281453+lorenzofattori@users.noreply.github.com>
2020-04-13 17:27:00 +0200

    fixed failing unit test

--- test/tests.py
+++ test/tests.py
@@ -3,16 +3,21 @@ from unittest.mock import patch
 
 from m2q_midi import probeMidiPorts
 
-class Test_M2Q_MIDI(unittest.TestCase):
 
+class Test_M2Q_MIDI(unittest.TestCase):
     @patch("m2q_midi.MidiIn")
     def test_no_midi_interface(self, mock_MidiIn):
-            mock_MidiIn().get_ports.return_value = []
-            ports = probeMidiPorts()
-            self.assertEqual(ports, [])
+        mock_MidiIn().get_ports.return_value = []
+        ports = probeMidiPorts()
+        self.assertEqual(ports, [])
 
     @patch("m2q_midi.MidiIn")
     def test_single_midi_interface(self, mock_MidiIn):
-            mock_MidiIn().get_ports.return_value = ['Fake midi']
-            ports = probeMidiPorts()
-            self.assertEqual(ports, ['Fake midi'])
+        mock_MidiIn().get_ports.return_value = ["loopMIDI Port 1 1"]
+        ports = probeMidiPorts()
+        self.assertEqual(ports, ["loopMIDI Port 1"])
+
+
+class Test_m2q_comm(unittest.TestCase):
+    def test_createMessage(self):
+        pass
